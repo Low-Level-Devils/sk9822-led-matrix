522f844a343fb2150e79138e01f8c0c95f49c9c1
Josh Walker <walkerje1992@gmail.com>
2026-01-08 17:22:38 -0700

    make animation_test flexible using args

--- sk9822-led/examples/animation_test.rs
+++ sk9822-led/examples/animation_test.rs
@@ -5,20 +5,42 @@ use std::thread;
 use std::time::Duration;
 
 const FPS: u64 = 1;
-const SLEEP_TIME: Duration = Duration::from_millis(1000 / FPS);
+const FRAMES: usize = 1;
 const LED_WIDTH: u8 = 14;
 const LED_HEIGHT: u8 = 14;
-const FRAMES: usize = 12;
+const ANIM_DIR: &str = "animations/";
 
 fn main() -> std::io::Result<()> {
-    let mut file = File::open("animations/color_test.bin").expect("color_test.bin not found");
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 3 {
+        eprintln!("SK9822 Animation Test");
+        eprintln!("Usage: {} <input> <frames> <optional fps>", args[0]);
+        std::process::exit(1);
+    }
+
+    if !args[1].ends_with(".bin") {
+        eprintln!("Unsupported input format: {}", args[1]);
+        std::process::exit(1);
+    }
+
+    let frames: usize = args[2].parse().unwrap_or(FRAMES);
+    let fps: u64 = if args.len() >= 4 {
+        args[3].parse().unwrap_or(FPS)
+    } else {
+        FPS
+    };
+
+    let sleep_time: Duration = Duration::from_millis(1000 / fps);
+
+    let mut file = File::open(ANIM_DIR.to_string() + &args[1]).expect("File not found");
     let mut buffer = [0u8; 4];
 
     let mut leds = Sk9822LedMatrix::new(LED_WIDTH, LED_HEIGHT);
     let _ = leds.connect("/dev/spidev0.0");
 
     loop {
-        for _frame in 0..FRAMES {
+        for _frame in 0..frames {
             for row in 0..LED_WIDTH {
                 for col in 0..LED_HEIGHT {
                     match file.read_exact(&mut buffer) {
@@ -35,7 +57,7 @@ fn main() -> std::io::Result<()> {
 
             leds.send_data()?;
 
-            thread::sleep(SLEEP_TIME);
+            thread::sleep(sleep_time);
         }
 
         file.seek(SeekFrom::Start(0))
