609bfafa6aa72e9120fd1a388a609bdc7b470671
Josh Walker <walkerje1992@gmail.com>
2026-01-12 10:09:24 -0700

    polish build script

--- build.py
+++ build.py
@@ -1,6 +1,9 @@
 import csv
 import subprocess
 import sys
+import os
+import shutil
+from pathlib import Path
 
 def convert_gifs_to_bin(csv_file):
     try:
@@ -45,31 +48,94 @@ def convert_gifs_to_bin(csv_file):
         sys.exit(1)
 
 
-def compile_binary(example_name):
-    command = [
-        'cargo', 'build', '--release',
-        '--example', example_name, 
-        '--target', 'aarch64-unknown-linux-gnu'
-    ]
+def compile_binaries():
+    try: 
+        dir_path = Path("sk9822-led/examples/")
 
-    print(f"Compiling binary with command: {' '.join(command)}")
+        if not dir_path.exists():
+            print("Directory sk9822-led/examples/ does not exist\n")
+            sys.exit(1)
+
+        files = list(dir_path.glob(f"*.rs"))
+
+        if not files:
+            print("No example files found")
+            return
+        
+        for file_path in files:
+            example_name = file_path.stem
+
+            command = [
+                'cargo', 'build', '--release',
+                '--example', example_name, 
+                '--target', 'aarch64-unknown-linux-gnu'
+            ]
+
+            print(f"Compiling binary with command: {' '.join(command)}")
     
-    try:
-        result = subprocess.run(
-            command,
-            capture_output=True,
-            text=True,
-            check=True
-        )
-        print("✓ Compilation successful")
-        if result.stdout:
-            print(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"✗ Compilation failed: {e}")
-        if e.stderr:
-            print(e.stderr)
+            try:
+                result = subprocess.run(
+                    command,
+                    capture_output=True,
+                    text=True,
+                    check=True
+                )
+                print("✓ Compilation successful")
+                if result.stdout:
+                    print(result.stdout)
+            except subprocess.CalledProcessError as e:
+                print(f"✗ Compilation failed: {e}")
+                if e.stderr:
+                    print(e.stderr)
+                sys.exit(1)
+
+            print("-" * 50)
+        
+    except Exception as e:
+        print(f"Error: {e}")
         sys.exit(1)
+
+
+def setup_output_dir():
+    output_path = Path("sk9822-led-module")
+
+    if output_path.exists():
+        shutil.rmtree(output_path)
+
+    output_path = output_path / Path("animations/")
+
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    print("sk9822-led-module dir created")
+    print("-" * 50)
+
+
+def copy_files():
+    gif_binaries_input_path = Path("sk9822-led/animations")
+    examples_binaries_input_path = Path("target/aarch64-unknown-linux-gnu/release/examples/")
+    output_path_animations = Path("sk9822-led-module/animations")
+    output_path_binaries = Path("sk9822-led-module/")
+
+    if not gif_binaries_input_path.exists():
+        print("Gif Binaries path sk9822-led/animations does not exist")
+        sys.exit(1)
+
+    shutil.copytree(gif_binaries_input_path, output_path_animations, dirs_exist_ok=True)
+
+    print("Animations copied successfully")
+    print("-" * 50)
+
+    files = [f for f in examples_binaries_input_path.iterdir() if f.is_file() and f.suffix == '' and not '-' in f.name]
+
+    for file_path in files:
+        destination = output_path_binaries / file_path.name
+        shutil.copy2(file_path, destination)
+
+    print("Binaries copied successfully")
+    print("-" * 50)
 
 if __name__ == "__main__":   
     convert_gifs_to_bin("manifest.csv")
-    compile_binary("animation_test")
+    compile_binaries()
+    setup_output_dir()
+    copy_files()
